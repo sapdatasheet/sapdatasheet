eccc1f4357186d2ef07d35b565c167e62b183b85
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2014-03-20 02:59:26 +0000

    PHPWeb Site - created

---
+++ trunk/PHPWebSite/abap/prog/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ trunk/PHPWebSite/phpinfo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <title>PHP Information</title>
+    <body>
+        <?php
+        phpinfo(); // Show all information, defaults to INFO_ALL
+        ?>
+    </body>
+</html>
+
