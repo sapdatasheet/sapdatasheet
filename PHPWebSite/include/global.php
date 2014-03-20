a39a9750cd247babec346001524685ae2b8c34a7
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2014-03-20 02:59:26 +0000

    PHPWeb Site - created

---
+++ PHPWebSite/include/global.php
@@ -0,0 +1,20 @@
+<?php
+
+ define(WEBSITE_NAME, "SAP Datasheet");
+ define(WEBSITE_DESC, "The Best Run SAP Run SAPDatasheet");
+ define(WEBSITE_TITLE, " - " . WEBSITE_NAME . " - " . WEBSITE_DESC);
+ define(WEBSITE_META_DESC, "Datasheet for all SAP objects: domain, data element, table, transaction code, etc");
+
+ define(ABAP_BMFR,  "Application Component");
+ define(ABAP_CVERS, "Software Component");
+ define(ABAP_DEVC,  "Package");
+ define(ABAP_DOMA,  "Domain");
+ define(ABAP_DTEL,  "Data Element");
+ define(ABAP_FUNC,  "Function Module");
+ define(ABAP_PROG,  "Cluster/Pool Table");
+ define(ABAP_SQLT,  "Package");
+ define(ABAP_TABL,  "Table");
+ define(ABAP_TRAN,  "Transaction Code");
+ define(ABAP_VIEW,  "View");
+
+ $earchTerm = "SAP";
