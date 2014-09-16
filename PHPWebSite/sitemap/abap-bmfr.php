bb5a749473b0d20d0a60577025ac7824e75b6d01
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2014-09-16 04:40:26 +0000

    PHPWeb Site - Sitemap - in progress

---
+++ PHPWebSite/sitemap/abap-bmfr.php
@@ -0,0 +1,25 @@
+<?php
+ob_start();
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once (__ROOT__ . '/include/abap_db.php');
+$bmfr = ABAP_DB_TABLE_HIER::DF14L_Sitemap();
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- ABAP BMFR - 5439 lines -->
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+    <?php while ($row = mysqli_fetch_array($bmfr)) { ?>
+    <url>
+        <loc>http://www.sapdatasheet.org/abap/bmfr/<?php echo strtolower($row['FCTR_ID']) ?>.html</loc> 
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <?php } ?>
+</urlset>
+<?php
+$ob_content = ob_get_contents();
+ob_end_flush();
+$ob_fp = fopen("./abap-bmfr.xml", "w");
+fwrite($ob_fp, $ob_content);
+fclose($ob_fp);
+
+
