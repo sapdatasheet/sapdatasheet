d29a97ebcca686e4876fec72127288a338d6d9af
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2014-10-15 05:54:42 +0000

    PHPWeb Site - Add Runtime scripts

---
+++ trunk/PHPWebSite/z-generate-buffer.cmd
@@ -0,0 +1,305 @@
+cd ..\www-root\abap\bmfr
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+pause
+
+cd ..\cvers
+php index.php
+pause
+
+cd ..\devc
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\doma
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\dtel
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\func
+php index.php
+php -f index.php rfc
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+php -f index.php 1
+php -f index.php 2
+php -f index.php 3
+php -f index.php 4
+php -f index.php 5
+php -f index.php 6
+php -f index.php 7
+php -f index.php 8
+php -f index.php 9
+pause
+
+cd ..\prog
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\sqlt
+php index.php
+pause
+
+cd ..\tabl
+php index.php
+php -f index.php cluster
+php -f index.php pool
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\tran
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+php -f index.php 1
+php -f index.php 2
+php -f index.php 3
+php -f index.php 4
+php -f index.php 5
+php -f index.php 6
+php -f index.php 7
+php -f index.php 8
+php -f index.php 9
+pause
+
+cd ..\view
+php index.php
+php -f index.php a
+php -f index.php b
+php -f index.php c
+php -f index.php d
+php -f index.php e
+php -f index.php f
+php -f index.php g
+php -f index.php h
+php -f index.php i
+php -f index.php j
+php -f index.php k
+php -f index.php l
+php -f index.php m
+php -f index.php n
+php -f index.php o
+php -f index.php p
+php -f index.php q
+php -f index.php r
+php -f index.php s
+php -f index.php t
+php -f index.php u
+php -f index.php v
+php -f index.php w
+php -f index.php x
+php -f index.php slash
+pause
+
+cd ..\..\sitemap
+php abap-bmfr.php
+php abap-cvers.php
+php abap-devc.php
+php abap-doma.php
+php abap-dtel.php
+php abap-func.php
+php abap-prog.php
+php abap-sqlt.php
+php abap-tabl.php
+php abap-table-field.php
+php abap-tran.php
+php abap-view.php
+php index.php
+php sitemaps.php
+pause
