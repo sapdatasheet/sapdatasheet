edb1e9b76ed5b69ec2f6e0b0399e0815dd734df8
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2014-10-18 03:39:01 +0000

    PHPWeb Site - Rename

--- www.sapdatasheet.org/z-deploy.cmd
+++ www.sapdatasheet.org/z-deploy.cmd
@@ -1,14 +1,15 @@
 cd C:\Data\Business\SAPDatasheet\Runtime\www-root
 del /S /Q *.php
-cd C:\Data\Business\SAPDatasheet\Development\Repos\PHPWebSite
+cd C:\Data\Business\SAPDatasheet\Development\Repos\www.sapdatasheet.org
 xcopy *.php      ..\..\..\Runtime\www-root /S /Y
 xcopy *.css      ..\..\..\Runtime\www-root /S /Y
 xcopy *.png      ..\..\..\Runtime\www-root /S /Y
 xcopy *.ico      ..\..\..\Runtime\www-root /S /Y
 xcopy *.txt      ..\..\..\Runtime\www-root /S /Y
-xcopy *.htaccess ..\..\..\Runtime\www-root /S /Y
 copy  .\admin\index.html  ..\..\..\Runtime\www-root\admin
 copy  .\include\footer.html  ..\..\..\Runtime\www-root\include
 copy  .\sitemap\index.html   ..\..\..\Runtime\www-root\sitemap
+mkdir ..\..\..\Runtime\www-root\site
+copy  .\site\*.html          ..\..\..\Runtime\www-root\site
 copy  z-generate-buffer.cmd  ..\..\..\Runtime\scripts
 pause
