04519e455d9266495622fc88286a503a41298c66
sapdatasheet@gmail.com <sapdatasheet@gmail.com@8f2d6d01-3fe5-a598-fa24-2527b423a1db>
2015-02-24 07:32:25 +0000

    PHPWeb Site - ABAP Analytics

--- www.sapdatasheet.org/include/abapana_db.php
+++ www.sapdatasheet.org/include/abapana_db.php
@@ -4,6 +4,9 @@ $__ROOT__ = dirname(dirname(__FILE__));
 require_once($__ROOT__ . '/include/config.php');
 
 class ABAPANA_DB_CONN {
+    
+    /** Data limit. */
+    const LIMIT = 1000;
 
     /** Schema Name. */
     const SCHEMA = 'abapanalytics';
@@ -15,17 +18,16 @@ class ABAPANA_DB_CONN {
     public static function get() {
         if (ABAPANA_DB_CONN::$conn == null) {
             ABAPANA_DB_CONN::$conn = new mysqli(
-                ABAP_DB_CONN::$host,
-                ABAP_DB_CONN::$user,
-                ABAP_DB_CONN::$pass,
-                ABAPANA_DB_CONN::SCHEMA);
+                    ABAP_DB_CONN::$host, ABAP_DB_CONN::$user, ABAP_DB_CONN::$pass, ABAPANA_DB_CONN::SCHEMA);
             ABAPANA_DB_CONN::$conn->set_charset("utf8");
         }
         return ABAPANA_DB_CONN::$conn;
     }
+
 }
 
 class ABAPANA_DB_TABLE {
+
     /**
      * Transaction Codes.
      */
@@ -42,4 +44,21 @@ class ABAPANA_DB_TABLE {
         $result = mysqli_fetch_array($qry);
         return $result;
     }
+
+    public static function TRAN_Rela($column, $value, $limit = FALSE) {
+        if (empty($column) or empty($value)) {
+            return null;
+        }
+        $con = ABAPANA_DB_CONN::get();
+        $column = $con->real_escape_string($column);
+        $value = $con->real_escape_string($value);
+        $sql = "SELECT tcode FROM " . ABAPANA_DB_TABLE::TRAN
+                . " where " . $column . " = '" . $value . "'"
+                . " order by tcode";
+        if ($limit == TRUE) {
+            $sql = $sql . ' limit ' . ABAPANA_DB_CONN::LIMIT;
+        }
+        return $con->query($sql);
+    }
+
 }
