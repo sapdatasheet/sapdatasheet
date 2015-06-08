da97c0a0df55ad610fc4499d5294d7b6d4716950
sapdatasheet <sapdatasheet@gmail.com>
2015-06-07 18:57:19 -0700

    Deploy Script - re-factory

--- Deploy/step2_bufferfile-clean.cmd
+++ Deploy/step2_bufferfile-clean.cmd
@@ -3,7 +3,7 @@ echo off
 cd C:\Data\Business\SAPDatasheet\Runtime\www-root\abap
 del /S *.html
 
-echo "Click any key to Continue delete site map files"
+echo "Click any key to Continue delete site map files or close window to cancel..."
 pause
 
 cd ..\sitemap
