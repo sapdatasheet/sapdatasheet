d8693adb9144c34135e504f934fcacdcd986b3d0
sapdatasheet <sapdatasheet@gmail.com>
2015-06-08 21:56:16 -0700

    www.sapdatasheet.org - Add I18N Support

--- www.sapdatasheet.org/include/header.js
+++ www.sapdatasheet.org/include/header.js
@@ -4,6 +4,7 @@ function langOnFocus(sapDescLangu) {
 function langOnChnage(sapDescLangu) {
     if (sapDescLangu._initValue !== sapDescLangu.value) {
         // change happened
+        clearCookie();
         setCookie('sap-desc-langu', sapDescLangu.value, 30);
 
         // Reload the Content page or Jump to Language Specific Index Page
@@ -65,8 +66,15 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/"; 
 }
+function clearCookie() {
+    var cookies = document.cookie.split(";");
 
-
-
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
