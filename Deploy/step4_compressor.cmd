49e10781ee3e0342af6538233041f90979b43ef9
sapdatasheet <sapdatasheet@gmail.com>
2015-11-05 22:54:19 -0800

    www.sapdatasheet.org - Sitemap

--- Deploy/step4_compressor.cmd
+++ Deploy/step4_compressor.cmd
@@ -5,6 +5,9 @@ cd C:\Data\Business\SAPDatasheet\Development\Repos\Deploy
 call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\bmfr
 FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\bmfr\%%i
 
+call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\clas
+FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\clas\%%i
+
 call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\cus0
 FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\cus0\%%i
 
@@ -26,8 +29,17 @@ FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Bus
 call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\func
 FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\func\%%i
 
+call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\intf
+FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\intf\%%i
+
+call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\msag
+FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\msag\%%i
+
 call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\prog
 FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\prog\%%i
+
+call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\shlp
+FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\shlp\%%i
 
 call:HtmlCompress                                               html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\sqlt
 FOR /F %%i IN (config-sap-desc-langu.txt) DO @call:HtmlCompress html C:\Data\Business\SAPDatasheet\Runtime\www-root\abap\sqlt\%%i
