4050b9861d7be30b59efbce497a05306c23e7ff1
SAP Datasheet <sapdatasheet@gmail.com>
2016-04-06 10:38:16 -0700

    Database - Analytic script update

--- MySQL/abapanalytics/table_abaptran.sql
+++ MySQL/abapanalytics/table_abaptran.sql
@@ -1,8 +1,13 @@
+-- Create Table
+
 CREATE TABLE `abaptran` (
   `TCODE` varchar(20) COLLATE utf8_bin NOT NULL COMMENT 'Transaction Code',
   `SOFTCOMP` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT 'Software Component',
-  `APPLPOSID` varchar(24) COLLATE utf8_bin DEFAULT NULL COMMENT 'Application component - PS_POSID',
-  `APPLFCTR` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT 'Application component - FCTR_ID',
+  `APPLPOSIDTOP1` varchar(24) COLLATE utf8_bin DEFAULT NULL COMMENT 'Top 1 level of the Application Component',
+  `APPLPOSIDTOP2` varchar(24) COLLATE utf8_bin DEFAULT NULL COMMENT 'Top 2 level of the Application Component',
+  `APPLPOSIDTOP3` varchar(24) COLLATE utf8_bin DEFAULT NULL COMMENT 'Top 3 level of the Application Component',
+  `APPLPOSID` varchar(24) COLLATE utf8_bin DEFAULT NULL COMMENT 'Application component - PS_POSID, Human readable format',
+  `APPLFCTR` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT 'Application component - FCTR_ID, Technical ID',
   `PACKAGE` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT 'Package',
   `PACKAGENS` varchar(10) COLLATE utf8_bin DEFAULT NULL COMMENT 'Package name space',
   `PACKAGEP` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT 'Package parent',
@@ -32,5 +37,40 @@ CREATE TABLE `abaptran` (
   `KEY7` varchar(132) COLLATE utf8_bin DEFAULT NULL,
   `VALUE7` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `DEBUG` varchar(254) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`tcode`)
+  PRIMARY KEY (`TCODE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Transaction Code';
+
+
+-- Prepare Data
+CALL `abapanalytics`.`abaptran_data`();
+
+-- Clean Data
+-- Clear Local System T-Codes
+DELETE FROM abapanalytics.abaptran
+  WHERE SOFTCOMP = 'LOCAL';
+
+-- Set the APPLPOSIDTOP1 column
+UPDATE abapanalytics.abaptran
+  SET APPLPOSIDTOP1 = SUBSTRING_INDEX(applposid, '-', 1)
+  WHERE applposid is not null;
+
+-- Set the APPLPOSIDTOP2 column
+UPDATE abapanalytics.abaptran
+  SET APPLPOSIDTOP2 = SUBSTRING_INDEX(applposid, '-', 2)
+  WHERE applposid is not null;
+
+UPDATE abapanalytics.abaptran
+  SET APPLPOSIDTOP2 = NULL
+  WHERE APPLPOSIDTOP2 is not null 
+    AND (length(APPLPOSIDTOP2)-length(replace(APPLPOSIDTOP2 ,'-',''))) <> 1;
+
+-- Set the APPLPOSIDTOP3 column
+UPDATE abapanalytics.abaptran
+  SET APPLPOSIDTOP3 = SUBSTRING_INDEX(applposid, '-', 3)
+  WHERE applposid is not null;
+
+UPDATE abapanalytics.abaptran
+  SET APPLPOSIDTOP3 = NULL
+  WHERE APPLPOSIDTOP3 is not null 
+    AND (length(APPLPOSIDTOP3)-length(replace(APPLPOSIDTOP3 ,'-',''))) <> 2;
+
